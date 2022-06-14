afcc917cb906d183eee45dd2c895ed54f3880587
QuestionMark001 <604661720@qq.com>
2022-06-14 20:41:35 +0800

    updated Ex

--- src/MCM/Simulated_Test.py
+++ src/MCM/Simulated_Test.py
@@ -1,1 +1,14 @@
+'''
+Author: QuestionMark001
+Date: 2022-06-13 20:42:59
+LastEditors: QuestionMark001
+LastEditTime: 2022-06-14 20:40:32
+FilePath: /LocalProjects/PythonEx/src/MCM/Simulated_Test.py
+Description: 全国数学建模竞赛--模拟题
+
+Copyright (c) 2022 by QuestionMark001, All Rights Reserved. 
+'''
+
+import os
+import pandas
 
